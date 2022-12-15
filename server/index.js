5f1b978b02b880909a6be424afabd202f440c028
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2022-12-15 15:29:15 -0700

    got my database set up and connected with sequalize

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
+const { sequelize } = require('./util/database')
+const { User } = require('./models/user')
+const { Post } = require('./models/post')
+
 const { PORT } = process.env
 
 const { login, logout, register } = require('./controllers/auth')
@@ -13,6 +17,10 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+
+User.hasMany(Post)
+Post.belongsTo(User)
 
 
 app.post('/register', register)
@@ -27,6 +35,13 @@ app.delete('/posts/:id', isAuthenticated , deletePost)
 
 
 
+//? the force: true is for development -- it DROPS tables!!!
+//? you can use it if you like while you are building
+//? sequelize.sync({ force: true })
 
+sequelize.sync() 
+.then(() => {
+    app.listen(PORT, () => console.log(`Listining on ${PORT}`)) //* we put this inside the .then becaues it will sync our database up before it starts the server. 
+})
+.catch((err) => console.log(err))
 
-app.listen(PORT, () => console.log(`Listining on ${PORT}`))
